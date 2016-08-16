88ee2ec66b926eb3f615c77ca98e757ef4f04f4c
tanggaolin <gaolin.tang@wenba100.com>
2016-08-16 18:11:43 +0800

    add script:ftp upload

---
+++ solution/solution.py
@@ -0,0 +1,12 @@
+class Solution(object):
+	def countSmaller(self,nums):
+		"""
+		:type nums : list[int]
+		:rtype :list[int]
+		"""
+		return  [1,2,3,4]
+
+
+solution = Solution()
+nums = [5,6,8,2,9,1,0,3]
+print solution.countSmaller(nums)
